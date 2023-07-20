9bc42fedf892ff9fcbe6c28a40f389083c6c46b8
SKhan003 <110658188+SKhan003@users.noreply.github.com>
2023-07-20 22:51:03 +0530

    #openChat,addChat

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,61 +1,63 @@
 var myBtn = document.querySelector("#myBtn");
 
-var searchNewUserBox = document.querySelector("#searchMainBox"); 
+var searchNewUserBox = document.querySelector("#searchMainBox");
 
 var closeSpan = document.querySelector(".close");
 
-var searchNewUserForm = document.querySelector("#findUser") 
+var searchNewUserForm = document.querySelector("#findUser");
 
-myBtn.addEventListener("click",function(){
-  searchNewUserBox.style.display = 'initial'
-  
-})
+myBtn.addEventListener("click", function () {
+  searchNewUserBox.style.display = "initial";
+});
 
 closeSpan.onclick = function () {
   searchNewUserBox.style.display = "none";
-}
+};
 
-
-var personalChat = document.querySelector("#personalchat");
+var allChat = document.querySelector("#allChat");
 var userRightChat = document.querySelector("#userChats");
 
-var currentOppositeUser = '';
+var currentOppositeUser = "";
 
-function addChat(username,image){
-  personalChat.innerHTML +=`<div onclick="openChat('${username}','${image}')" class="personalchat">
-  <div class="personalProfile">
-    <img src="${image}" alt="">
-  </div>
-  <h3>${username}</h3>
-</div>`
+function addChat(username, image) {
+  allChat.innerHTML += `<div id="personalchat" class="personalchat" onclick="openChat(${username},${image})">
+        <div class="personalProfile">
+            <img src="${image}" alt="">
+          </div>
+              <h3>${username}</h3>
+          </div>`;
 }
 
-
-async function openChat(username,image){
-  currentOppositeUser = username
-  userRightChat.innerHTML = `<div class="rightNav">
-  <div class="rightUser">
-    <img src=${image} alt="">
-    <h2>${username}</h2>
+function openChat(username,image) {
+  userRightChat.innerHTML = ` <div id="rightUserNav">
+  <div class="rightUserImage"><img src="${image}" alt=""></div>
+  <h2>${username}</h2>
+</div>
+<div id="usersConversation">
+</div>
+<div id="rightBottomInput">
+  <div class="sme">
+    <i class="ri-emotion-happy-line"></i>
   </div>
+  <div class="sme">
+    <i class="ri-attachment-2"></i>
   </div>
-  <div class="conversation">
-    
+  <textarea placeholder="Type a message"></textarea>
+  <div class="sendbtn sme">
+    <i class="ri-send-plane-line"></i></a>
   </div>
-   <input type="text" onchange="newmsg()" id="newmsg" placeholder="start new conversation" />
-  `
+</div>`;
 }
 
-searchNewUserBox.addEventListener('submit',async (event)=>{
+searchNewUserBox.addEventListener("submit", async (event) => {
   event.preventDefault();
   var userDetail = document.querySelector("#newUsersearch");
-  var responce = await axios.post('/findUser',{
-    data:userDetail.value
-  })
-  if(responce.data.isUserThere){
-    addChat(responce.data.user.username, responce.data.user.pic)
+  var responce = await axios.post("/findUser", {
+    data: userDetail.value,
+  });
+  if (responce.data.isUserThere) {
+    addChat(responce.data.user.username, responce.data.user.pic);
+  } else {
+    alert("no user found");
   }
-  else{
-    alert('no user found');
-  }
-})
+});
